e75f478fe0cf1198669bca531431857e01bca59c
Deepanjali Mishra <71490671+codver@users.noreply.github.com>
2021-07-04 13:03:55 +0530

    Update external_led_blink.py

--- Getting Started/external_led_blink.py
+++ Getting Started/external_led_blink.py
@@ -1,3 +1,7 @@
+''' External LED was connected to Pin 15 of the Raspberry Pi Pico'''
+''' A resistor of 100 ohms was used'''
+
+
 from machine import Pin, Timer
 led = Pin(15, Pin.OUT)
 timer = Timer()
@@ -5,5 +9,4 @@ timer = Timer()
 #function
 def blink(timer):
     led.toggle()
-#timer.init(fre=2.5, mode=Timer.PERIODIC)
 timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
